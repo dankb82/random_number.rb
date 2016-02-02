411a8fb2fddc201d90760a45fad70584dd236917
Daniel Buompane <danielbuompane@gmail.com>
2016-02-02 13:44:21 -0500

    addedfirst level of guessing game.

--- random_number.rb
+++ random_number.rb
@@ -1,6 +1,12 @@
 def number
-  puts "here is a number."
-  return rand(100)
+  return rand(1..2)
 end
 
+puts "Please guess a number from 1 to 2"
+guess = gets.chomp.to_i
 puts number
+  if guess != number
+    puts "please guess another number."
+  else
+    puts "You win!"
+  end
