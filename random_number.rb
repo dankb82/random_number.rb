b28cba0ce0792eeb84a7542df7670870ded34e84
Daniel Buompane <danielbuompane@gmail.com>
2016-02-02 16:10:55 -0500

    fixed array bug. Completed normal mode.

--- random_number.rb
+++ random_number.rb
@@ -1,6 +1,6 @@
 
 def ask
-  puts "Please guess a number from 1 to 5"
+  puts "Please guess a number from 1 to 100"
 return gets.chomp
 end
 
@@ -9,17 +9,20 @@ def attempts
 end
 
 attempts = 0
-answer = rand(1..5)
+answer = rand(1..100)
 guesses = []
 guess = 0
 
 until attempts == 5 || guess == answer
 guess = ask.to_i
-guesses << guess
-  if guess < answer
+  if guesses.include? (guess.to_i)
+    puts "You guessed that, dummy. Pay attention!"
+  elsif guess < answer
+    guesses << guess
     attempts += 1
     puts "Your guess is too low."
   elsif guess > answer
+    guesses << guess
     attempts += 1
     puts "Your guess is too high."
   else
